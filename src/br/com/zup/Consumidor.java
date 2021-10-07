91b566dfb783b3a44b1e9cc11fa546f59a9241b6
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-07 15:50:04 -0300

    Moldar classe Consumidor

--- src/br/com/zup/Consumidor.java
+++ src/br/com/zup/Consumidor.java
@@ -1,4 +1,49 @@
 package br.com.zup;
 
 public class Consumidor {
+    private String nome;
+    private String email;
+    private TipoPessoa tipoPessoa;
+
+    public Consumidor() {
+    }
+
+    public Consumidor(String nome, String email, TipoPessoa tipoPessoa) {
+        this.nome = nome;
+        this.email = email;
+        this.tipoPessoa = tipoPessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TipoPessoa getTipoPessoa() {
+        return tipoPessoa;
+    }
+
+    public void setTipoPessoa(TipoPessoa tipoPessoa) {
+        this.tipoPessoa = tipoPessoa;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder concatenador = new StringBuilder();
+        concatenador.append("\tNome: " + nome);
+        concatenador.append("\tEmail: " + email);
+        concatenador.append("\tPessoa " + tipoPessoa);
+        return concatenador.toString();
+    }
 }
