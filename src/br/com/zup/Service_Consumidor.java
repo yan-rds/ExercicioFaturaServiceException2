a9fe8f752f7ea5dc25dc962dca11376eebaa6bb7
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-07 15:54:24 -0300

    Instanciar listas de Consumidor e Fatura

--- src/br/com/zup/Service_Consumidor.java
+++ src/br/com/zup/Service_Consumidor.java
@@ -1,4 +1,9 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Service_Consumidor {
+    private List<Consumidor> listaDeConsumidor = new ArrayList<>();
+
 }
--- src/br/com/zup/Service_Fatura.java
+++ src/br/com/zup/Service_Fatura.java
@@ -1,4 +1,9 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Service_Fatura {
+    private List<Fatura> listaDeFatura = new ArrayList<>();
+
 }
