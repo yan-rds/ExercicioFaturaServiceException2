09735ae17f8c8f7d63a2c3addf27517539e6177d
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-07 15:45:05 -0300

    Moldar classe Fatura

--- src/br/com/zup/Fatura.java
+++ src/br/com/zup/Fatura.java
@@ -1,4 +1,50 @@
 package br.com.zup;
 
 public class Fatura {
+    private Consumidor consumidor;
+    private double valor;
+    private String dataDeVencimento;
+
+    public Fatura() {
+    }
+
+    public Fatura(Consumidor consumidor, double valor, String dataDeVencimento) {
+        this.consumidor = consumidor;
+        this.valor = valor;
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public Consumidor getConsumidor() {
+        return consumidor;
+    }
+
+    public void setConsumidor(Consumidor consumidor) {
+        this.consumidor = consumidor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(String dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder concatenador = new StringBuilder();
+        concatenador.append("\n\t\tDados da Fatura");
+        concatenador.append("\nConsumidor: " + consumidor);
+        concatenador.append("\nValor: " + valor);
+        concatenador.append("\nData de Vencimendo: " + dataDeVencimento);
+        return concatenador.toString();
+    }
 }
